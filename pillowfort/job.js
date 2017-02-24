25bd62ba8ce70af91eb0bc5fe8866a8d41b3a94d
Jasper St. Pierre <jstpierre@mecheye.net>
2017-02-23 18:06:35 -0800

    don't put protobuf in hatch manifest

--- pillowfort/job.js
+++ pillowfort/job.js
@@ -1,6 +1,5 @@
 
 const crypto = require('crypto');
-const SomaProto = require('soma-proto');
 
 function new_job_id() {
     const hash = crypto.createHash('sha1');
@@ -10,25 +9,16 @@ function new_job_id() {
 
 class SimpleImageJob {
     constructor(uri) {
-        this._job_header = SomaProto.JobHeader.from({
-            id: new_job_id(),
-            please_queue: false,
-
-            job_type: SomaProto.JobType.values.INGEST_SIMPLE_IMAGE,
-            ingest_simple_image_payload: SomaProto.IngestSimpleImagePayload.from({
-                uri: uri,
-            }),
-        });
+        this._job_id = new_job_id();
+        this._uri = uri;
     }
 
-    get job_id() { return this._job_header.id; }
+    get job_id() { return this._job_id; }
     // duck-type
     get asset_id() { return this.job_id; }
 
     _to_hatch_manifest() {
-        const proto = SomaProto.JobHeader.encode(this._job_header).finish();
-        const proto64 = proto.toString('base64');
-        return { "job_id": this.job_id, "proto": proto64 };
+        return { "job_id": this.job_id, "type": "simple_image", "uri": this._uri };
     }
 }
 
