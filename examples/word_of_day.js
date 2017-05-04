f0a2dd2339c010ca516c4c083b962ca538abb130
Syndacks <math.milliken@gmail.com>
2017-05-04 15:41:19 -0400

    Add synopsis method to NewsArticle class

--- examples/word_of_day.js
+++ examples/word_of_day.js
@@ -8,22 +8,19 @@ const url = require('url');
 
 const home_page = 'http://www.thefreedictionary.com/_/archive.htm'; // Home section
 
-
 function ingest_article_profile(hatch, uri) {
     return libingester.util.fetch_html(uri).then(($profile) => {
         const base_uri = libingester.util.get_doc_base_uri($profile, uri);
-        const asset = new libingester.NewsArticle();
 
+        const asset = new libingester.NewsArticle();
         asset.set_canonical_uri(uri);
 
-        // use url library to pull out query string date object
+        // Use 'url' module to pull out query string date object
         const parts = url.parse(uri, true);
         const modified_date = new Date(Date.parse(parts.query.d));
-
         asset.set_last_modified_date(modified_date);
-        asset.set_section("word_of_day");
 
-        //Set title section
+        // Set date and title section
         const date = parts.query.d;
         const title = $profile('h1').first().text() + ": " + date;
 
@@ -33,14 +30,12 @@ function ingest_article_profile(hatch, uri) {
 
         // Pluck wordOfDayDef
         const wordOfDayMixedString = $profile('td:contains("Definition:")').next().text();
-        const wordOfDayDef = wordOfDayMixedString.split(" ").slice(1).join(" ");
-
+        const wordOfDayDef = wordOfDayMixedString.slice(wordOfDayMixedString.indexOf(" "));
         asset.set_synopsis(wordOfDayDef);
 
         // Pluck wordOfDayType
-        const pos = wordOfDayMixedString.split(" ")[0];
-        const wordOfDayType = pos.substr(1, pos.length-2);
-
+        const wordTypeBlob = wordOfDayMixedString.split(" ")[0];
+        const wordOfDayType = wordTypeBlob.substr(1, wordTypeBlob.length-2);
         asset.set_license(wordOfDayType);
 
         const content = mustache.render(template.structure_template, {
@@ -50,11 +45,11 @@ function ingest_article_profile(hatch, uri) {
             wordOfDay: wordOfDay,
             wordOfDayType: wordOfDayType,
             wordOfDayDef: wordOfDayDef,
-
-            sourceURL: uri
         });
 
         asset.set_document(content);
+        asset.set_section("word_of_day");
+
         hatch.save_asset(asset);
     });
 }
@@ -63,9 +58,8 @@ function main() {
     const hatch = new libingester.Hatch();
     // make request to the index (home) page
     libingester.util.fetch_html(home_page).then(($pages) => {
-        // retrieve all the URLs for individual pages
+      // retrieve article URLs; '-2n+2' returns ~30 articles instead of 2,000+
         const articles_links = $pages('#Calendar div:nth-child(-2n+2) a').map(function() {
-        // const articles_links = $pages('#Calendar div:nth-child(-n+15) a').map(function() {
             const uri = $pages(this).attr('href');
             return url.resolve(home_page, uri);
         }).get();
