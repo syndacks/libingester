0ae5586f4252a160002132a8a970028ba0e219cc
Syndacks <math.milliken@gmail.com>
2017-05-04 15:42:23 -0400

    Add word_of_day ingester and template

---
+++ examples/word_of_day_template.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const structure_template = (`
+<section class="title">
+  <h1 id="title">{{ title }}</h1>
+  <p id="date">{{ date }}</p>
+</section>
+
+<br/>
+
+<section class="mainContent">
+
+  <h3>Word of the Day</h3>
+    <h4 id="wordOfDay">{{ wordOfDay }}</h4>
+    <p id="wordofDayType"> <i>{{ wordOfDayType }}</i></p>
+    <p id="wordofDayDef">Definition: {{ wordOfDayDef }}</p>
+    <br/>
+
+    <p id="sourceURL">Source URL: {{ sourceURL }}</p>
+</section>`);
+
+exports.structure_template = structure_template;
