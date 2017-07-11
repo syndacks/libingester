8941d5dad7b30744df83da6de7f1774c5961dbd3
Syndacks <math.milliken@gmail.com>
2017-07-11 13:22:04 -0400

    Update ingester with hybrid mmethod of JSON API and scraping

--- examples/wiki_art.js
+++ examples/wiki_art.js
@@ -10,7 +10,7 @@ const RSS_BASE_2 = 'https://www.wikiart.org/en/profile/594bd184edc2c91224abfcd5/
 
 
 function ingest_art(hatch, artwork_metadata) {
-    const uri = url.resolve(base_uri, artwork_metadata.id)
+    const uri = url.resolve(base_uri, artwork_metadata.paintingUrl)
     return libingester.util.fetch_html(uri).then(($profile) => {
 
         const asset = new libingester.GalleryImageArticle();
@@ -18,24 +18,40 @@ function ingest_art(hatch, artwork_metadata) {
         asset.set_author(artwork_metadata.artistName)
         asset.set_title(artwork_metadata.title);
         asset.set_canonical_uri(uri);
-        asset.set_date_published('9999');
 
-        // Pull out the updated date
+        // Pull out date_published
+        const date_published = artwork_metadata.year;
+        // there is some bad data here for date_published, need to handle
+        if (date_published !== "?" && date_published.length === 4) {
+          asset.set_date_published(date_published);
+        } else {
+          asset.set_date_published("9999");
+        }
+
         asset.set_last_modified_date(new Date());
-        asset.set_tags(['tag']);
+
+        if ($profile){
+          const body = $profile('div.info div.info-line').nextAll().map(function(){
+            return $profile(this);
+          }).get().join(' ');
+
+          const tags = $profile('span[itemprop=keywords] a').map((i, elem) => $profile(elem).text()).get();
+          asset.set_body(body);
+          asset.set_tags(tags);
+
+        } else {
+          asset.set_body('');
+          asset.set_tags(['wiki_art']);
+        }
 
         //ensure cheerio value
-        asset.set_body('');
-        asset.set_read_more_text('');
+        asset.set_read_more_text('www.wikiart.org');
 
-        // asset.set_as_static_page()
         // image
-        const image_description = $profile(".image-wrapper .comment").children();
         const main_image = libingester.util.download_image(artwork_metadata.image);
         asset.set_thumbnail(main_image);
         asset.set_main_image(main_image, artwork_metadata.title);
         hatch.save_asset(main_image);
-
 
         asset.render();
         hatch.save_asset(asset);
@@ -55,8 +71,6 @@ function main() {
     }).then((art_json) => {
         return Promise.all(art_json.map((art_metadata) => ingest_art(hatch, art_metadata)));
     });
-
-
 
     Promise.all([artworks])
            .then(() => hatch.finish());
