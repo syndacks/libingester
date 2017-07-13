36b3a13411cc5232b3f0502434d1587e398f597a
Syndacks <math.milliken@gmail.com>
2017-07-13 16:13:36 -0400

    Nature is speaking updates

--- examples/nature_is_speaking.js
+++ examples/nature_is_speaking.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const libingester = require('libingester');
-const mustache = require('mustache');
-const request = require('request');
 const rp = require('request-promise');
-// const template_artist = require('./template_artist');
-// const template_artwork = require('./template_artwork');
 const url = require('url');
 
 const base_uri = "http://www.conservation.org";
@@ -25,6 +21,76 @@ const nature_elements = [
     'url' : 'http://www.conservation.org/nature-is-speaking/pages/harrison-ford-is-the-ocean.aspx',
     'video_uri' : 'http://embed.wistia.com/deliveries/d23c3bbc754f25c8937a2bd4adf63456ebdb5ad6/file.mp4',
     'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/ocean_bg_medium.jpg',
+  },
+  {
+    'title' : 'Sky',
+    'author' : 'Joan Chen',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Joan-Chen-is-Sky.aspx',
+    'video_uri' : 'http://embed.wistia.com/deliveries/449ff34cb1f0f64318b063b33817f75b8d7784e1/file.mp4',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/sky_bg_medium.jpg',
+  },
+  {
+    'title' : 'Home',
+    'author' : 'Reese Witherspoon',
+    'url' : 'http://www.conservation.org/nature-is-speaking/pages/reese-witherspoon-is-home.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=mkjwxmcdb0E',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/home_bg_medium.jpg',
+  },
+  {
+    'title' : 'Mountain',
+    'author' : 'Lee Pace',
+    'url' : 'http://www.conservation.org/nature-is-speaking/pages/lee-pace-is-mountain.aspx',
+    'video_uri' : 'http://embed.wistia.com/deliveries/1a6272d420e827fc10be088c0b3a34d6bac501d9/file.mp4',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/mountain_bg_medium.jpg',
+  },
+  {
+    'title' : 'Flower',
+    'author' : 'Lee Pace',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Lupita-Nyongo-Is-Flower.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=0_OxI2JZex4',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/flower_bg_medium.jpg',
+  },
+  {
+    'title' : 'Coral Reef',
+    'author' : 'Ian Somerhalder',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Ian-Somerhalder-Is-Coral-Reef.aspx',
+    'video_uri' : 'http://embed.wistia.com/deliveries/d89c2531c6def6e2b0890e997d7c16c06fa3a1c2/file.mp4',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/coral_bg_medium.jpg',
+  },
+  {
+    'title' : 'The Redwood',
+    'author' : 'Roberd Redford',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Ian-Somerhalder-Is-Coral-Reef.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=3e66bnuxV2A',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/redwood_bg_medium.jpg',
+  },
+  {
+    'title' : 'Mother Nature',
+    'author' : 'Julia Roberts',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Julia-Roberts-Is-Mother-Nature.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=WmVLcj-XKnM',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/mothernature_bg_medium.jpg',
+  },
+  {
+    'title' : 'The Soil',
+    'author' : 'Edward Norton',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Edward-Norton-Is-the-Soil.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=Dor4XvjA8Wo',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/soil_bg_medium.jpg',
+  },
+  {
+    'title' : 'The Rainforest',
+    'author' : 'Kevin Spacey',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Kevin-Spacey-Is-the-Rainforest.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=jBqMJzv4Cs8',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/rainforest_bg_medium.jpg',
+  },
+  {
+    'title' : 'Water',
+    'author' : 'Penelope Cruz',
+    'url' : 'http://www.conservation.org/nature-is-speaking/Pages/Penelope-Cruz-Is-Water.aspx',
+    'video_uri' : 'https://www.youtube.com/watch?v=fwV9OYeGN88',
+    'image_uri' : 'http://www.conservation.org/nature-is-speaking/publishingimages/water_bg_medium.jpg',
   },
 ];
 
